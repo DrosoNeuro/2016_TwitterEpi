b3345dfb5f2676597d09281709426abf511c0c51
Servan Grüninger <servan.grueninger@uzh.ch>
2017-01-13 16:56:54 +0100

    fixed timezone lookup - slow

--- Non_R_Code/TimeZoneFinder/TimeZoneLookUp.sh
+++ Non_R_Code/TimeZoneFinder/TimeZoneLookUp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 source ~/anaconda3/bin/activate ~/anaconda3/envs/rpython
-python TimeZoneLookUp.py
+#file is going to be opened from within R environment with paths that might be different, so make sure absolute paths are correct!
+python /home/drosoneuro/Dropbox/UZH_Master/Masterarbeit/TwitterEpi/Non_R_Code/TimeZoneFinder/TimeZoneLookUp.py
+
